5764569ea584fcc8db4be6f3cd2558a6b8770bb9
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-15 09:31:40 +0530

    Add files via upload

---
+++ Rescue_Mission/Rescue Mission.py
@@ -0,0 +1,19 @@
+Nof_hideouts = int(input())
+hides = input().split()
+
+soldiers = 0
+
+for i in hides:
+    soldiers += int(i)
+
+days = int(input())
+rescue = 0
+
+for i in range(days):
+    vals = input().split()
+    rescue += int(vals[2])
+
+if soldiers < rescue:
+    print(soldiers)
+else:
+    print(rescue)
