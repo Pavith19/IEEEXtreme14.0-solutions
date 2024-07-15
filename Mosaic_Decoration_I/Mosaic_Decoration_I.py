6cef2065fdb5f61a54ceb29b76d8d4374f5391f3
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-15 09:26:14 +0530

    Add files via upload

---
+++ Mosaic_Decoration_I/Mosaic_Decoration_I.py
@@ -0,0 +1,18 @@
+import math
+inp = list(map(int,input().split()))
+ts = inp[0]
+cb = inp[1]
+cp = inp[2]
+# print(ts)
+# print(cb)
+res = 0
+bt = 0
+pt = 0
+for i in range(ts):
+    tiles = list(map(int,input().split()))
+    bt  = bt + tiles[0]
+    pt  = pt + tiles[1]
+tbt = math.ceil(bt/10)
+tpt = math.ceil(pt/10)
+print(tbt*cb+tpt*cp)
+    
